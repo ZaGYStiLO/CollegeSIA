50330b04475cbf13984cb2b337f32c71dc5fd7a7
ivan <ivansevryukov358@gmail.com>
2026-03-19 13:22:56 +0100

    add services

---
+++ CollegeSIA/Services/IScheduleService.cs
@@ -0,0 +1,10 @@
+﻿using CollegeSIA.DTO;
+
+namespace CollegeSIA.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime
+startDate, DateTime endDate);
+    }
+}
